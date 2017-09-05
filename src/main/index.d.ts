56d2a04f8a3c2ca6334f6da5f627667690f9ecad
luzzif <fedeluzzi00@gmail.com>
2017-09-05 16:36:54 +0200

    Package.json review

---
+++ src/main/index.d.ts
@@ -0,0 +1,12 @@
+export { BuyLimitRequest } from "./request/BuyLimitRequest";
+export { MarketSummaryRequest } from "./request/MarketSummaryRequest";
+export { MarketUpdatesStreamRequest } from "./request/MarketUpdatesStreamRequest";
+
+export { Buy } from "./response/Buy";
+export { BuyLimitResponse } from "./response/BuyLimitResponse";
+export { Fill } from "./response/Fill";
+export { MarketSummaryResponse } from "./response/MarketSummaryResponse";
+export { MarketUpdate } from "./response/MarketUpdate";
+export { Sell } from "./response/Sell";
+
+export { BittrexApiClient } from "./BittrexApiClient";
