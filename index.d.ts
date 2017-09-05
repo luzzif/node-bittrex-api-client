cbc5e706e09573c04f85128a0ef57993da272d27
luzzif <fedeluzzi00@gmail.com>
2017-09-05 15:46:59 +0200

    Update package exports

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,10 @@
+export { MarketSummaryRequest } from "./src/main/request/MarketSummaryRequest";
+export { MarketUpdatesStreamRequest } from "./src/main/request/MarketUpdatesStreamRequest";
+
+export { Buy } from "./src/main/response/Buy";
+export { Fill } from "./src/main/response/Fill";
+export { MarketSummaryResponse } from "./src/main/response/MarketSummaryResponse";
+export { MarketUpdate } from "./src/main/response/MarketUpdate";
+export { Sell } from "./src/main/response/Sell";
+
 export { BittrexApiClient } from "./src/main/BittrexApiClient";
