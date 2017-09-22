c878d2411db3a3f1e88bf1aefe433633c65f76f1
luzzif <fedeluzzi00@gmail.com>
2017-09-22 21:30:39 +0200

    Replace native promises implementation with bluebird, adding more flexibility

--- src/main/BittrexApiClient.ts
+++ src/main/BittrexApiClient.ts
@@ -14,6 +14,7 @@ import { OpenOrder } from "./model/OpenOrder";
 import { Balance } from "./model/Balance";
 import { ExchangeStateUpdate } from "./model/ExchangeStateUpdate";
 import { isNullOrUndefined } from "util";
+import * as Promise from "bluebird";
 
 export class BittrexApiClient {
 
