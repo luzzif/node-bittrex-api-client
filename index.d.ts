b4efb39b796bb495e0585ef51ea7bede0134d4a1
luzzif <fedeluzzi00@gmail.com>
2017-10-14 14:38:09 +0200

    Fix index.d.ts exports

--- index.d.ts
+++ index.d.ts
@@ -8,10 +8,13 @@ export { ResponseParsingError } from "./src/main/error/ResponseParsingError";
 export { Balance } from "./src/main/model/Balance";
 export { Currency } from "./src/main/model/Currency";
 export { ExchangeStateUpdate } from "./src/main/model/ExchangeStateUpdate";
+export { Fill } from "./src/main/model/Fill";
 export { Market } from "./src/main/model/Market";
 export { MarketSummary } from "./src/main/model/MarketSummary";
 export { OpenOrder } from "./src/main/model/OpenOrder";
 export { Order } from "./src/main/model/Order";
+export { OrderBookOrder } from "./src/main/model/OrderBookOrder";
+export { PlacedOrder } from "./src/main/model/PlacedOrder";
 export { Ticker } from "./src/main/model/Ticker";
 export { Trade } from "./src/main/model/Trade";
 
